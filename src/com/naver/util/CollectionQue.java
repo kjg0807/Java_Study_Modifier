e381f5d04c201551c3cefbefbf4850b759b6915b
kimjinkyu <kjg08070@naver.com>
2022-07-18 17:55:55 +0900

    Collection_final

--- src/com/naver/util/CollectionQue.java
+++ src/com/naver/util/CollectionQue.java
@@ -1,10 +1,36 @@
 package com.naver.util;
 
-public interface CollectionQue extends Collection
+public class CollectionQue implements Collection
 {
 	// 2. com.naver.util.CollectionQue
 	// add - 새로운 숫자 추가 무조건 0번 인덱스에 추가 1,2,3에 4를 넣으면 -> 4,1,2,3
 	// remove - 마지막 인덱스 삭제
-	
-	
+
+	@Override
+	public int[] add(int[] numbers)
+	{
+		// add - 새로운 숫자 추가 무조건 0번에 추가
+		int num = 4; // 추가할 Data
+		int[] newAr = new int[numbers.length + 1];
+
+		newAr[0] = num; // 0 번 index에 추가
+		for (int i = 0; i < numbers.length; i++)
+		{
+			newAr[i + 1] = numbers[i];
+		}
+
+		return newAr;
+	}
+
+	@Override
+	public int[] remove(int[] numbers)
+	{
+		// remove - 마지막 인덱스 번호 삭제
+		int[] newAr = new int[numbers.length - 1];
+		for (int i = 0; i < newAr.length; i++)
+		{
+			newAr[i] = numbers[i];
+		}
+		return newAr;
+	}
 }
--- src/com/naver/util/CollectionSet.java
+++ src/com/naver/util/CollectionSet.java
@@ -1,10 +1,86 @@
 package com.naver.util;
 
-public interface CollectionSet extends Collection
+public class CollectionSet implements Collection
 {
 	// 3. com.naver.util.CollectionSet
 	// add - 새로운 숫자 마지막에 추가 단, 중복되지 않는 숫자만 추가
 	// remove - 삭제할 숫자를 입력받아서 일치하는 숫자를 삭제
-	
-	
+
+	@Override
+	public int[] add(int[] numbers)
+	{
+		// add - 새로운 숫자 추가 마지막에 추가
+		// 단, 중복되지 않는 숫자만 추가
+		int num = 3; // 추가할 Data
+		int i = 0; // 중복 확인
+		for (i = 0; i < numbers.length; i++)
+		{
+			if (num == numbers[i])
+			{
+				// 중복 발생
+				break;
+			}
+		}
+		// i 값이 numbers.length보다 작은 경우 중복 Data가 있는 경우
+		// 아니면 중복이 없는 경우
+
+		if (i < numbers.length)
+		{
+			// 중복된 데이터가 있는 경우
+			// Data를 추가하지 않고 기존 Data를 리턴
+			return numbers;
+		}
+		else
+		{
+			// 중복된 데이터가 없는 경우
+			// Data를 추가하고 리턴
+			int[] newAr = new int[numbers.length + 1];
+
+			for (i = 0; i < numbers.length; i++)
+			{
+				newAr[i] = numbers[i];
+			}
+			newAr[numbers.length] = num;
+
+			return newAr;
+		}
+
+	}
+
+	@Override
+	public int[] remove(int[] numbers)
+	{
+		// remove - 삭제할 숫자를 입력받아서 일치하는 숫자 삭제
+		int num = 1; // 삭제할 Data;
+		int i = 0; // 삭제할 Data 확인
+		for (i = 0; i < numbers.length; i++)
+		{
+			if (num == numbers[i])
+			{
+				// 삭제할 Data와 일치
+				break;
+			}
+		}
+
+		if (i < numbers.length)
+		{
+			// 삭제할 data를 제외하고 배열 복사
+			int[] newAr = new int[numbers.length - 1];
+			int index = 0; // newAr index번호용
+
+			for (int j = 0; j < numbers.length; j++)
+			{
+				if (j == i)
+				{
+					continue;
+				}
+				newAr[index] = numbers[j];
+				index++;
+
+			}
+			return newAr;
+		}
+
+		return numbers;
+	}
 }
